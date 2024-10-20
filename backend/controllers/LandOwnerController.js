20f3dc0659fb87cfd17bbc793441a7b99d55e83e
Rana Faraz <ranafaraznaru@gmail.com>
2024-10-20 18:01:15 +0500

    unnecessary code removed

--- backend/controllers/LandOwnerController.js
+++ backend/controllers/LandOwnerController.js
@@ -1,272 +1,9 @@
 import prisma from "../DB/db.config.js";
 import fs from "fs";
-import csv from "csv-parser";
-import { makePostRequest1 } from "../utils/api.js";
 import FormData from "form-data";
 import { makePostRequest } from "../services/makeRequest.js";
 
 class LandOwnerController {
-  // static async uploadCSV(req, res) {
-  //   try {
-  //     const file = req.file;
-  //     const user = req.user;
-
-  //     if (!file) {
-  //       return res.status(400).json({ message: "No file uploaded" });
-  //     }
-
-  //     const landOwnersData = [];
-
-  //     // Wrap CSV parsing in a promise to await its completion
-  //     const parseCSV = () => {
-  //       return new Promise((resolve, reject) => {
-  //         fs.createReadStream(file.path)
-  //           .pipe(csv())
-  //           .on("data", (row) => {
-  //             landOwnersData.push({
-  //               name: row.name || null,
-  //               address: row.address || null,
-  //               cityState: row.cityState || null,
-  //               email: row.email || null,
-  //               phone: row.phone || null,
-  //             });
-  //           })
-  //           .on("end", () => resolve())
-  //           .on("error", (err) => reject(err));
-  //       });
-  //     };
-
-  //     await parseCSV();
-
-  //     // After parsing CSV, update the user's LandOwners array
-  //     const userId = user.id;
-
-  //     // Fetch current user from DB
-  //     const findUser = await prisma.users.findUnique({
-  //       where: { id: userId },
-  //       include: { LandOwners: true },
-  //     });
-
-  //     if (!findUser) {
-  //       return res.status(404).json({ message: "User not found" });
-  //     }
-
-  //     // Save the updated landowners array in the DB
-  //     await Promise.all(
-  //       landOwnersData.map((owner) => {
-  //         return prisma.LandOwners.create({
-  //           data: {
-  //             name: owner.name,
-  //             address: owner.address,
-  //             cityState: owner.cityState,
-  //             email: owner.email,
-  //             phone: owner.phone,
-  //             user: {
-  //               connect: { id: userId }, // Assuming user with id 1 exists
-  //             },
-  //           },
-  //         });
-  //       })
-  //     );
-
-  //     // Optionally, you could remove the uploaded file from the server
-  //     fs.unlinkSync(file.path);
-
-  //     return res.json({ message: "Landowners added successfully" });
-  //   } catch (error) {
-  //     return res.status(500).json({
-  //       status: 500,
-  //       message: "Something went wrong. Please try again.",
-  //     });
-  //   }
-  // }
-  // static async uploadCSV(req, res) {
-  //   try {
-  //     const file = req.file;
-  //     const user = req.user;
-
-  //     if (!file) {
-  //       return res.status(400).json({ message: "No file uploaded" });
-  //     }
-
-  //     const landOwnersData = [];
-
-  //     // Parse CSV
-  //     const parseCSV = () => {
-  //       return new Promise((resolve, reject) => {
-  //         fs.createReadStream(file.path)
-  //           .pipe(csv())
-  //           .on("data", (row) => {
-  //             landOwnersData.push({
-  //               name: row.name || null,
-  //               address: row.address || null,
-  //               cityState: row.cityState || null,
-  //               email: row.email || null,
-  //               phone: row.phone || null,
-  //             });
-  //           })
-  //           .on("end", () => resolve())
-  //           .on("error", (err) => reject(err));
-  //       });
-  //     };
-
-  //     await parseCSV();
-
-  //     // Fetch user ID
-  //     const userId = user.id;
-
-  //     // Scraper function to fetch phone and email
-
-  //     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  //     // Save landowners
-  //     await Promise.all(
-  //       landOwnersData.map(async (owner, index) => {
-  //         const searchUrl = `${
-  //           process.env.TRUE_PEOPLE_SEARCH_URL
-  //         }/results?name=${encodeURIComponent(
-  //           owner.name
-  //         )}&citystatezip=${encodeURIComponent(owner.cityState)}`;
-  //         await delay(2 * 15000);
-  //         const searchPageData = await makeScraperRequest(searchUrl);
-  //         // console.log("Search Page Data:", searchPageData);
-  //         const detailPageUrl = extractDetailPageUrl(searchPageData);
-  //         // console.log("detailPageUrl", detailPageUrl);
-  //         // If email or phone is missing, fetch from scraper
-  //         if ((!owner.email || !owner.phone) && detailPageUrl) {
-  //           await delay(1 * 15000);
-  //           const detailPageData = await makeScraperRequest(detailPageUrl);
-  //           // console.log("Detail Page Data:", detailPageData);
-
-  //           // Extract phone and email
-  //           const phone = extractPhone(detailPageData);
-  //           const emails = extractEmail(detailPageData);
-  //           const email = Array.isArray(emails) ? emails[0] : emails;
-  //           owner.phone = phone || owner.phone;
-  //           owner.email = email || owner.email;
-  //         }
-
-  //         // Save landowner data to database
-  //         return prisma.LandOwners.create({
-  //           data: {
-  //             name: owner.name,
-  //             address: owner.address,
-  //             cityState: owner.cityState,
-  //             email: owner.email,
-  //             phone: owner.phone,
-  //             user: {
-  //               connect: { id: userId }, // Assuming user with id exists
-  //             },
-  //           },
-  //         });
-  //       })
-  //     );
-
-  //     // Optionally, remove the uploaded file from the server
-  //     fs.unlinkSync(file.path);
-
-  //     return res.json({ message: "Landowners added successfully" });
-  //   } catch (error) {
-  //     console.log("Error uploading CSV:", error);
-  //     return res.status(500).json({
-  //       status: 500,
-  //       message: "Something went wrong. Please try again.",
-  //     });
-  //   }
-  // }
-  // static async uploadCSV(req, res) {
-  //   try {
-  //     const file = req.file;
-  //     const user = req.user;
-
-  //     if (!file) {
-  //       return res.status(400).json({ message: "No file uploaded" });
-  //     }
-
-  //     const landOwnersData = [];
-
-  //     // Parse CSV
-  //     const parseCSV = () => {
-  //       return new Promise((resolve, reject) => {
-  //         fs.createReadStream(file.path)
-  //           .pipe(csv())
-  //           .on("data", (row) => {
-  //             landOwnersData.push({
-  //               name: row.name || null,
-  //               address: row.address || null,
-  //               cityState: row.cityState || null,
-  //               email: row.email || null,
-  //               phone: row.phone || null,
-  //             });
-  //           })
-  //           .on("end", () => resolve())
-  //           .on("error", (err) => reject(err));
-  //       });
-  //     };
-
-  //     await parseCSV();
-
-  //     // Fetch user ID
-  //     const userId = user.id;
-
-  //     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  //     // Process each landowner sequentially with 7-second delay between requests
-  //     for (const owner of landOwnersData) {
-  //       const searchUrl = `${
-  //         process.env.TRUE_PEOPLE_SEARCH_URL
-  //       }/results?name=${encodeURIComponent(
-  //         owner.name
-  //       )}&citystatezip=${encodeURIComponent(owner.cityState)}`;
-
-  //       // Wait for 7 seconds before each request
-  //       await delay(7 * 1000);
-
-  //       const searchPageData = await makeScraperRequest(searchUrl);
-
-  //       const detailPageUrl = extractDetailPageUrl(searchPageData);
-  //       console.log("detailPageUrl", detailPageUrl);
-  //       // If email or phone is missing, fetch from scraper
-  //       if ((!owner.email || !owner.phone) && detailPageUrl) {
-  //         await delay(7 * 1000);
-  //         const detailPageData = await makeScraperRequest(detailPageUrl);
-
-  //         // Extract phone and email
-  //         const phone = extractPhone(detailPageData);
-  //         const emails = extractEmail(detailPageData);
-  //         const email = Array.isArray(emails) ? emails[0] : emails;
-
-  //         owner.phone = phone || owner.phone;
-  //         owner.email = email || owner.email;
-  //       }
-
-  //       // Save landowner data to database
-  //       await prisma.LandOwners.create({
-  //         data: {
-  //           name: owner.name,
-  //           address: owner.address,
-  //           cityState: owner.cityState,
-  //           email: owner.email,
-  //           phone: owner.phone,
-  //           user: {
-  //             connect: { id: userId }, // Assuming user with id exists
-  //           },
-  //         },
-  //       });
-  //     }
-
-  //     // Optionally, remove the uploaded file from the server
-  //     fs.unlinkSync(file.path);
-
-  //     return res.json({ message: "Landowners added successfully" });
-  //   } catch (error) {
-  //     console.log("Error uploading CSV:", error);
-  //     return res.status(500).json({
-  //       status: 500,
-  //       message: "Something went wrong. Please try again.",
-  //     });
-  //   }
-  // }
-
   static async uploadCSV(req, res) {
     try {
       const file = req.file;
@@ -285,69 +22,29 @@ class LandOwnerController {
         });
       }
 
-      const landOwnersData = [];
+      const formData = new FormData();
+      formData.append("address_column", "Address");
+      formData.append("city_column", "Home City");
+      formData.append("state_column", "State");
+      formData.append("first_name_column", "First Name");
+      formData.append("last_name_column", "Last Name");
+      formData.append("mail_address_column", "Owner Mailing Address");
+      formData.append("mail_city_column", "Owner Mailing City");
+      formData.append("mail_state_column", "Owner Mailing State");
+      formData.append("csv_file", fs.createReadStream(file.path), {
+        filename: file.filename,
+        contentType: file.mimetype,
+      });
 
-      // Parse CSV and validate columns
-      const parseCSV = () => {
-        return new Promise((resolve, reject) => {
-          fs.createReadStream(file.path)
-            .pipe(csv())
-            .on("data", (row) => {
-              landOwnersData.push({
-                address: row.Address || null,
-                city: row["Home City"] || null,
-                state: row.State || null,
-                firstName: row["First Name"] || null,
-                lastName: row["Last Name"] || null,
-              });
-            })
-            .on("end", () => resolve())
-            .on("error", (err) => reject(err));
-        });
-      };
-      await parseCSV();
-
-      // console.log("landOwnersData", landOwnersData);
-      for (const owner of landOwnersData) {
-        const formData = new FormData();
-        formData.append("address_column", owner.address);
-        formData.append("city_column", owner.city);
-        formData.append("state_column", owner.state);
-        formData.append("first_name_column", owner.firstName);
-        formData.append("last_name_column", owner.lastName);
-        formData.append("mail_address_column", owner.address);
-        formData.append("mail_city_column", owner.city);
-        formData.append("mail_state_column", owner.state);
-        // formData.append("csv_file", file);
-        console.log("file", file);
-        // console.log("filepath", file.path);
-        const fileStream = fs.createReadStream(file.path);
-        // formData.append("csv_file", fileStream);
-        formData.append("csv_file", fs.createReadStream(file.path), {
-          filename: file.originalname,
-          contentType: file.mimetype,
-        });
-
-        const response = await makePostRequest(formData);
-        // const config = {
-        //   headers: { "Content-Type": "multipart/form-data" },
-        // };
-        const config = {
-          headers: formData.getHeaders(), // Include FormData headers
-        };
-        const url = `${process.env.TRACERFY_BASE_URL}/trace/`;
-
-        // const response = await makePostRequest1(url, formData, config);
-
-        console.log("API response:", response);
-      }
+      const response = await makePostRequest(formData);
+      console.log("Third party response", response);
 
       // Optionally, remove the uploaded file from the server
       fs.unlinkSync(file.path);
 
       return res.json({ message: "CSV processed successfully" });
     } catch (error) {
-      console.log("Error uploading CSV:", error);
+      console.error("Error uploading CSV:", error);
       return res.status(500).json({
         status: 500,
         message: "Something went wrong. Please try again.",
--- backend/services/makeRequest.js
+++ backend/services/makeRequest.js
@@ -4,9 +4,6 @@ const apiEndpoint = `${process.env.TRACERFY_BASE_URL}/trace/`;
 const bearerToken = process.env.TRACERFY_API_KEY;
 
 export async function makePostRequest(formData) {
-  // console.log("bearerToken", bearerToken);
-  // console.log("apiEndpoint", apiEndpoint);
-
   const config = {
     method: "post",
     maxBodyLength: Infinity,
@@ -19,48 +16,9 @@ export async function makePostRequest(formData) {
   };
   try {
     const response = await axios(config);
-    console.log("Response:", response.status, response.data);
     return response;
   } catch (error) {
-    // console.log("Error:", error.response ? error.response.data : error.message);
     console.log("error", error);
     throw error;
   }
 }
-
-// import axios from 'axios';
-// import FormData from 'form-data';
-// import fs from 'fs';
-
-// const data = new FormData();
-// data.append('address_column', 'Address');
-// data.append('city_column', 'City');
-// data.append('state_column', 'State');
-// data.append('first_name_column', 'Owner 1 First Name');
-// data.append('last_name_column', 'Owner 1 Last Name');
-// data.append('mail_address_column', 'Address');
-// data.append('mail_city_column', 'City');
-// data.append('mail_state_column', 'State');
-
-// // Append CSV file
-// data.append('csv_file', fs.createReadStream('/Users/reaper/Documents/desktop clone/Propwire Export - 601 Properties - Apr 29_ 2024.csv'));
-
-// // Axios config
-// const config = {
-//   method: 'post',
-//   maxBodyLength: Infinity,
-//   url: `${process.env.TRACERFY_BASE_URL}/trace/`,
-//   headers: {
-//     ...data.getHeaders(),
-//   },
-//   data: data,
-// };
-
-// // Send request using axios
-// axios(config)
-//   .then((response) => {
-//     console.log(JSON.stringify(response.data));
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
